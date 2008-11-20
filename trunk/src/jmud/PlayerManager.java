8d40bfaa41d4dcfa5edd7eb7307cd059a758c725
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-20 09:22:37 +0000

    Added Comments!

--- trunk/src/jmud/PlayerManager.java
+++ trunk/src/jmud/PlayerManager.java
@@ -1,6 +1,16 @@
 package jmud;
 /*
- * ToDo: add some notes in here as to what problem this is going to solve
+ * Currently PlayerChannel contains all the requisite
+ * info for a players in-game state AND all info required
+ * for sending and receiving data from the client.
+ * The List<PlayerChannel> is now maintained in a
+ * Thread subclass that is also of Stereotype: Control.  
+ * 
+ * To improve thread safety and data accessibility,
+ * Move PlayerChannel lists/maps to a 
+ * Singleton Class (PlayerManager)
+ * anchored to the root executable class and conform this
+ * class to the Stereotype: Entity as much as practical. 
  *
  */
 
