a63cbf7555e240ecb39d8b068118b520a3fd5a10
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-21 20:48:53 +0000

    Working on streamlining the jmud.netIO package.  Added a prototype ConnectionAttempt class.  The first of a few classes in an attempt to capture a real-world login process via classing.  Proof of Concept work.

---
+++ src/jmud/netIO/ConnectionAttempt.java
@@ -0,0 +1,18 @@
+package jmud.netIO;
+/**
+ * AcceptThread spawns a ConnectionAttempt object for each attempted connection.
+ * ConnectionAttempt object will spawn ConnectionAttemptJob objects which will attempt
+ * to authenticate against the DB and return a boolean.
+ * ConnectionAttempt objects will track the number of times an account has attempted to
+ * log in and will handle the text sending to the client.
+ * 
+ * Once the ConnectionAttempt object has attained a successful Authentication from the DB via
+ * ConnectionAttempJob objects, a Player object is spawned and added to the PlayerManager.
+ *
+ * @author David Loman
+ * @version 0.1
+
+ */
+public class ConnectionAttempt {
+
+}
