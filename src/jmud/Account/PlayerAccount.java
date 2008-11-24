048c19b24d2974e7d0a4957023ec6b33e781fbc2
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-24 17:18:28 +0000

    Basic Framework for PlayerAccount Management.  A PlayerAccount will hold data pertaining to a Players Account, e.g. uName, passwd, email, character list, etc.

---
+++ src/jmud/Account/PlayerAccount.java
@@ -0,0 +1,64 @@
+package jmud.Account;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jmud.character.Character;
+
+/**
+ * 
+ * 24NOV08:  PlayerAccount should only represent the data that pertains to
+ * Account only, aka, username, email and password plus any settings they
+ * might have, aka Telnet settings.
+ * 
+ * A playerAccount will point to many Characters.
+ * 
+ * Created on 24NOV08
+ */
+
+
+
+
+public class PlayerAccount {
+   private int playerAccountID = -1;
+
+    // validation
+    private String uname = "";
+    private String password = "";
+    private String emailAddress = "";
+    
+    private Set<Character> characters = new HashSet<Character>();
+
+    
+  
+	public PlayerAccount(String emailAddress, String password, int playerAccountID, String uname) {
+		super();
+		this.emailAddress = emailAddress;
+		this.password = password;
+		this.playerAccountID = playerAccountID;
+		this.uname = uname;
+	}
+
+	public int getPlayerAccountID() {
+		return playerAccountID;
+	}
+
+	public String getUname() {
+		return uname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public Set<Character> getCharacters() {
+		return characters;
+	}
+    
+
+    
+}
---
+++ src/jmud/Account/PlayerAccountManager.java
@@ -0,0 +1,85 @@
+package jmud.Account;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * PlayerAccountManager is a Singleton patterned class designed to store and
+ * facilitate easy lookup of Online PlayerAccounts.
+ *
+ */
+
+public class PlayerAccountManager {
+	/* 
+	 * ********************************************
+	 * Singleton Implementation
+	 * ********************************************
+	 */	
+	
+	
+	/**
+	 * Protected constructor is sufficient to suppress unauthorized calls to the
+	 * constructor
+	 */
+	protected PlayerAccountManager() {
+	}
+
+	/**
+	 * PlayerAccountManagerHolder is loaded on the first execution of
+	 * PlayerAccountManager.getInstance() or the first access to
+	 * PlayerAccountManagerHolder.INSTANCE, not before.
+	 */
+	private static class PlayerAccountManagerHolder {
+		private final static PlayerAccountManager INSTANCE = new PlayerAccountManager();
+	}
+
+	public static PlayerAccountManager getInstance() {
+		return PlayerAccountManagerHolder.INSTANCE;
+	}
+
+	/* 
+	 * ********************************************
+	 * Concrete Class Implementation
+	 * ********************************************
+	 */	
+	
+	
+	private HashSet<PlayerAccount> accountSet = new HashSet<PlayerAccount>();
+	private HashMap<String, PlayerAccount> uNameMap = new HashMap<String, PlayerAccount>();
+	private HashMap<String, PlayerAccount> emailMap = new HashMap<String, PlayerAccount>();
+	
+	public void addPlayerAccount(PlayerAccount pa) {
+		this.accountSet.add(pa);
+		this.uNameMap.put(pa.getUname(), pa);
+		this.emailMap.put(pa.getEmailAddress(), pa);
+		
+	}
+	
+	public void remPlayerAccount(PlayerAccount pa) {
+		this.accountSet.remove(pa);
+		this.uNameMap.remove(pa.getUname());
+		this.emailMap.remove(pa.getEmailAddress());
+	}
+	
+	public PlayerAccount getPlayerAccountByEmail(String email) {
+		return this.emailMap.get(email);
+	}
+	
+	public PlayerAccount getPlayerAccountByUName(String uname) {
+		return this.uNameMap.get(uname);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
