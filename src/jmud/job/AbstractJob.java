919a7079c2069aa2d81eff332552f5fab586a98b
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-24 14:20:41 +0000

    Added the ground work for the Job handling system.
    JobManager is a singleton patterned class.  It contains the queue of jobs to be executed by n number of JobWorker objects.
    
    JobWorker objects will implement Runnable and will be controlled by JobManager.
    
    AbstractJob class provides the base mandatory implementation of a Job.  ALL jobs are to extend AbstractJob.  (Consider making this fully virtual)
    
    CheckConnBufferForValidCmd_Job class:  A job created by the ConnectionManager to test to see if the contained Connection object's readBuffer contains a complete jMUD command yet.  If it does, then it is parsed out of the ByteBuffer and a subsequent BuildCmdFromString_Job is generated.  If not, then the Connection object's readBuffer is left untouched.
    
    BuildCmdFromString_Job class:  A Job created to see if a jMUD command can be built into a command object.  This is accomplished by checking the String to jMudCommand map in the CommandManager.

--- src/jmud/job/AbstractJob.java
+++ src/jmud/job/AbstractJob.java
@@ -1,11 +1,26 @@
 package jmud.job;
 
-/*
- * AbstractJob.java
+/**
+ * Provides mandatory base implementation for all 'Job's'
  *
- * Created on 18NOV08 by David Loman
+ * @author David Loman
+ * @version 0.1
  */
-
 public abstract class AbstractJob {
 
+	private long JobID = -1L;
+
+	public abstract boolean doJob();
+
+	public long getJobID() {
+		return JobID;
+	}
+
+	public void setJobID(long jobID) {
+		JobID = jobID;
+	}
+
+	public void submitSelf() {
+		JobManager.getInstance().pushJobToQueue(this);
+	}
 }
