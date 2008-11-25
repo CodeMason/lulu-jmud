7a94dd8936d565c7adc2889b426636846b2e67ac
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-25 11:51:11 +0000

    Laid down framework for configuring the jmud: AbstractConfig and ConfigManager
    In short, since nearly all the *Managers in jmud rely on single instances of Definition classes to be registered with a Manager/Registrar (StatDef, SlotDef, etc) an easy way is needed to instantiate these instances and load them into the manager.  The config system will do that by providing a Base AbstractConfig that the mud Dev extends from and populates the .doConfig() method with the code that instantiates and registers their Def classes.  The ConfigManager.executeConfigs() method is called at the very startup of the mud.

---
+++ trunk/src/jmud/engine/config/AbstractConfig.java
@@ -0,0 +1,32 @@
+package jmud.engine.config;
+
+/**
+ *
+ * 25NOV08:  AbstractConfig is the base, abstract class that mandates
+ * the minimum required operations for a subclass.  The config system
+ * is used to load in all the single instance subclasses for the 
+ * various *Managers in jmud.
+ *
+ *
+ *
+ *
+ * Created on April 21, 2002, 4:24 PM
+ */
+
+
+
+
+public abstract class AbstractConfig {
+	protected String name = "";
+	public abstract boolean doConfig();
+
+	public AbstractConfig(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
