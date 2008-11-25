b690b882694d6c23c6173525ddd68f704a13cb81
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-25 17:05:15 +0000

    Package prep for continuing Item/Slots system work.

---
+++ src/jmud/testserver/itemcontainerdefs/Foot.java
@@ -0,0 +1,29 @@
+/*
+ * Hand.java
+ *
+ * Created on December 9, 2007, 5:05 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package jmud.testserver.itemcontainerdefs;
+
+/**
+ * @author root
+ */
+public class Foot extends SingleSlot {
+    private static final int MAX_BULK = 20;
+
+    /**
+     * Creates a new instance of Foot
+     */
+    public Foot(int Id, String name) {
+        super(Id, name);
+    }
+
+    public int maxBulk() {
+        return MAX_BULK;
+    }
+
+}
---
+++ src/jmud/testserver/itemcontainerdefs/MultiSlot.java
@@ -0,0 +1,81 @@
+/*
+ * Hand.java
+ *
+ * Created on December 9, 2007, 5:05 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package jmud.testserver.itemcontainerdefs;
+
+import jmud.engine.item.AbstractItemDef;
+import jmud.engine.slot.Slot;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author root
+ */
+public abstract class MultiSlot extends Slot {
+    public static final int MAX_BULK = 10;
+    public static final int MAX_ITEMS = 2;
+    private List<AbstractItemDef> items;
+    private String name;
+
+    /**
+     * Creates a new instance of Finger
+     *
+     * @param name name of the slot
+     */
+    public MultiSlot(int Id, String name) {
+        super(Id, name);
+        items = new LinkedList<AbstractItemDef>();
+    }
+
+    public final int itemCount() {
+        return items.size();
+    }
+
+    public boolean isFull() {
+        return itemCount() == MAX_ITEMS;
+    }
+
+    public boolean isEmpty() {
+        return itemCount() == 0;
+    }
+
+    public final boolean hasItem(AbstractItemDef item) {
+        return items.contains(item);
+    }
+
+    public final boolean addItem(AbstractItemDef item) {
+        if(items.size() < MAX_ITEMS) {
+            items.add(item);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Go through all the items and return the first one that matches the name
+     */
+    public final AbstractItemDef removeItem(String name) {
+        for(AbstractItemDef i : items) {
+            if(i.getName().equals(name)) {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public final List<AbstractItemDef> getItems() {
+        return items;
+    }
+
+    public boolean isGrabber() {
+        return false;
+    }
+
+}
