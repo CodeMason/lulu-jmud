1c36c6d07db198c4c305ff185a1c2df404b81e43
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-25 17:05:15 +0000

    Package prep for continuing Item/Slots system work.

---
+++ trunk/src/jmud/testserver/itemcontainerdefs/Finger.java
@@ -0,0 +1,41 @@
+/*
+ * Finger.java
+ *
+ * Created on December 9, 2007, 5:11 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package jmud.testserver.itemcontainerdefs;
+
+import jmud.engine.item.AbstractItemDef;
+import jmud.testserver.itemcontainerdefs.MultiSlot;
+
+import java.util.List;
+
+/**
+ * @author root
+ */
+public class Finger extends MultiSlot {
+    public static final int MAX_BULK = 10;
+    public static final int MAX_ITEMS = 2;
+    private List<AbstractItemDef> items;
+    private String name;
+
+    /**
+     * Creates a new instance of Finger
+     */
+    public Finger(int Id, String name) {
+        super(Id, name);
+    }
+
+    public int maxBulk() {
+        return MAX_BULK;
+    }
+
+    public int maxItems() {
+        return MAX_ITEMS;
+    }
+
+}
