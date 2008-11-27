3cf7686ad535b9cc73a894653a2daab755897175
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-11-27 20:33:11 +0000

    Added eventID, eventType and List<JMudObject> targets to JMudEvent.

--- src/jmud/engine/event/JMudEvent.java
+++ src/jmud/engine/event/JMudEvent.java
@@ -1,11 +1,17 @@
 package jmud.engine.event;
 
+import java.util.List;
+import java.util.UUID;
+
 import jmud.engine.object.JMudObject;
 
-public class JMudEvent{
+public class JMudEvent {
 
+	private UUID eventID = null;
+	private int eventType = -1;
+	
     protected transient JMudObject source;
-
+    protected List<JMudObject> targets;
     /**
      * Constructs a prototypical Event.
      *
@@ -22,6 +28,25 @@ public class JMudEvent{
      * @return The object on which the Event initially occurred.
      */
     public JMudObject getSource(){
-        return source;
+        return this.source;
     }
+    
+    /**
+     * The objects on which the Event is targeted.
+     *
+     * @return The object on which the Event initially occurred.
+     */
+    public List<JMudObject> getTargets(){
+        return this.targets;
+    }
+
+	public UUID getEventID() {
+		return this.eventID;
+	}
+
+	public int getEventType() {
+		return eventType;
+	}
+    
+    
 }
