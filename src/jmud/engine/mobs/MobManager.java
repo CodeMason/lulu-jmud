5c72da497e8565483e449dbf84aea1b246d8de5c
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-01 23:15:33 +0000

    Applying initial pass of Checkstyle.

--- src/jmud/engine/mobs/MobManager.java
+++ src/jmud/engine/mobs/MobManager.java
@@ -2,95 +2,67 @@ package jmud.engine.mobs;
 
 import java.util.HashMap;
 
-
-
 /**
- * Singleton patterned class
- * Manages all Mob objects.
- *
+ * Singleton patterned class Manages all Mob objects.
  * @author David Loman
  * @version 0.1
  */
 
 public class MobManager {
-	/*
-	 * ********************************************
-	 * Singleton Implementation
-	 * ********************************************
-	 */
-	/**
-	 * Protected constructor is sufficient to suppress unauthorized calls to the
-	 * constructor
-	 */
-	protected MobManager() {
-	}
+   /**
+    * MobManagerHolder is loaded on the first execution of
+    * MobManager.getInstance() or the first access to MobManagerHolder.INSTANCE,
+    * not before.
+    */
+   private static class MobManagerHolder {
+      private static final MobManager INSTANCE = new MobManager();
+   }
 
-	/**
-	 * MobManagerHolder is loaded on the first execution of
-	 * MobManager.getInstance() or the first access to
-	 * MobManagerHolder.INSTANCE, not before.
-	 */
-	private static class MobManagerHolder {
-		private static final MobManager INSTANCE = new MobManager();
-	}
+   public static MobManager getInstance() {
+      return MobManagerHolder.INSTANCE;
+   }
 
-	public static MobManager getInstance() {
-		return MobManagerHolder.INSTANCE;
-	}
+   private final HashMap<Integer, Mob> mobMap = new HashMap<Integer, Mob>();
 
-	/*
-	 * ********************************************
-	 * Concrete Class Implementation
-	 * ********************************************
-	 */
+   /*
+    * Concrete Class Implementation
+    */
 
-	private final HashMap<Integer, Mob> mobMap = new HashMap<Integer, Mob>();
+   /*
+    * Singleton Implementation
+    */
+   /**
+    * Protected constructor is sufficient to suppress unauthorized calls to the
+    * constructor
+    */
+   protected MobManager() {
+   }
 
+   /*
+    * Queue Access
+    */
+   public final void addMob(final Mob m) {
+      synchronized (this.mobMap) {
+         this.mobMap.put(m.getID(), m);
+      }
+   }
 
-	public void init () {
+   public final Mob getMob(final int ID) {
+      Mob r = null;
+      synchronized (this.mobMap) {
+         r = this.mobMap.get(ID);
+      }
+      return r;
+   }
 
-	}
+   public void init() {
 
-	/*
-	 * ********************************************
-	 * Queue Access
-	 * ********************************************
-	 */
-	public void addMob(Mob m) {
-		synchronized (this.mobMap) {
-			this.mobMap.put(m.getID(), m);
-		}
-	}
+   }
 
-	public Mob getMob(int ID) {
-		Mob r = null;
-		synchronized (this.mobMap) {
-			r = this.mobMap.get(ID);
-		}
-		return r;
-	}
-
-	public void remMob(Mob m) {
-		synchronized (this.mobMap) {
-			this.mobMap.remove(m.getID());
-		}
-	}
+   public final void remMob(final Mob m) {
+      synchronized (this.mobMap) {
+         this.mobMap.remove(m.getID());
+      }
+   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
