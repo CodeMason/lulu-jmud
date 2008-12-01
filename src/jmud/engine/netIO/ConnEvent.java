56b0d08fe2a99dc7d7bc6c9f862e46627586cc69
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-01 22:57:11 +0000

    Applying Checkstyle and Javadoc.

--- src/jmud/engine/netIO/ConnEvent.java
+++ src/jmud/engine/netIO/ConnEvent.java
@@ -2,17 +2,21 @@ package jmud.engine.netIO;
 
 import java.nio.channels.SocketChannel;
 
+/**
+ * @author David Loman
+ */
 public class ConnEvent {
-  public static final int REGISTER = 1;
-  public static final int CHANGEOPS = 2;
-  
-  public SocketChannel socket;
-  public int type;
-  public int ops;
-  
-  public ConnEvent(SocketChannel socket, int type, int ops) {
-    this.socket = socket;
-    this.type = type;
-    this.ops = ops;
-  }
+   public static final int REGISTER = 1;
+   public static final int CHANGEOPS = 2;
+
+   public SocketChannel socket;
+   public int type;
+   public int ops;
+
+   public ConnEvent(final SocketChannel inSocket, final int inType,
+         final int inOps) {
+      this.socket = inSocket;
+      this.type = inType;
+      this.ops = inOps;
+   }
 }
