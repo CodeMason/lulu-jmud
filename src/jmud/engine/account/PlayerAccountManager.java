a4b420d3a5b8f504ac458f8858d19ddece39ea1d
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-02 16:47:01 +0000

    Added/revised Javadoc.

--- src/jmud/engine/account/PlayerAccountManager.java
+++ src/jmud/engine/account/PlayerAccountManager.java
@@ -4,44 +4,65 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * PlayerAccountManager is a Singleton patterned class designed to store and
- * facilitate easy lookup of Online PlayerAccounts.
+ * <code>PlayerAccountManager</code> is a singleton patterned class designed to
+ * store and facilitate easy lookup of online <code>PlayerAccounts</code>.
+ * @author david.h.loman
  */
-
 public class PlayerAccountManager {
-   /*
-    * Singleton Implementation
-    */
 
    /**
-    * PlayerAccountManagerHolder is loaded on the first execution of
-    * PlayerAccountManager.getInstance() or the first access to
-    * PlayerAccountManagerHolder.INSTANCE, not before.
+    * <code>PlayerAccountManagerHolder</code> is loaded on the first execution
+    * of <code>PlayerAccountManager.getInstance()</code> or the first access to
+    * <code>PlayerAccountManagerHolder.INSTANCE</code>, not before.
     */
-   private static class PlayerAccountManagerHolder {
+   private static final class PlayerAccountManagerHolder {
+      /**
+       * The singleton instance.
+       */
       private static final PlayerAccountManager INSTANCE = new PlayerAccountManager();
+
+      /**
+       * <code>PlayerAccountManagerHolder</code> is a utility class. Disallowing
+       * public/default constructor.
+       */
+      private PlayerAccountManagerHolder() {
+
+      }
    }
 
+   /**
+    * @return the singleton instance of the <code>PlayerAccountManager</code>
+    */
    public static PlayerAccountManager getInstance() {
       return PlayerAccountManagerHolder.INSTANCE;
    }
 
+   /**
+    * A <code>HashSet</code> of <code>PlayerAccounts</code>.
+    */
    private final HashSet<PlayerAccount> accountSet = new HashSet<PlayerAccount>();
 
-   /*
-    * Concrete Class Implementation
+   /**
+    * A <code>HashMap</code> for e-mail addresses.
     */
-
-   private final HashMap<String, PlayerAccount> uNameMap = new HashMap<String, PlayerAccount>();
    private final HashMap<String, PlayerAccount> emailMap = new HashMap<String, PlayerAccount>();
+   /**
+    * A <code>HashMap</code> for usernames.
+    */
+   private final HashMap<String, PlayerAccount> uNameMap = new HashMap<String, PlayerAccount>();
 
    /**
     * Protected constructor is sufficient to suppress unauthorized calls to the
-    * constructor
+    * constructor.
     */
    protected PlayerAccountManager() {
    }
 
+   /**
+    * Adds a new <code>PlayerAccount</code>.
+    * @param pa
+    *           the account to add
+    */
    public final void addPlayerAccount(final PlayerAccount pa) {
       this.accountSet.add(pa);
       this.uNameMap.put(pa.getUname(), pa);
@@ -49,14 +70,31 @@ public class PlayerAccountManager {
 
    }
 
+   /**
+    * Get a player's account using the user's e-mail address as search criteria.
+    * @param email
+    *           the query criteria
+    * @return a player's account as specified by <code>email</code>
+    */
    public final PlayerAccount getPlayerAccountByEmail(final String email) {
       return this.emailMap.get(email);
    }
 
+   /**
+    * Get a player's account using the username as search criteria.
+    * @param uname
+    *           the query criteria
+    * @return the player's account as specified by <code>uname</code>
+    */
    public final PlayerAccount getPlayerAccountByUName(final String uname) {
       return this.uNameMap.get(uname);
    }
 
+   /**
+    * Remove a player's account.
+    * @param pa
+    *           the account to remove
+    */
    public final void remPlayerAccount(final PlayerAccount pa) {
       this.accountSet.remove(pa);
       this.uNameMap.remove(pa.getUname());
