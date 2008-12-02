7225372d361b026264aec8ec48a82b6e89622970
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-02 17:22:05 +0000

    Added/revised Javadoc.

--- src/jmud/engine/character/Character.java
+++ src/jmud/engine/character/Character.java
@@ -8,52 +8,88 @@ import jmud.engine.item.ItemContainer;
 import jmud.engine.stats.StatMap;
 
 /**
- * 24NOV08: Character objects should only represent the data that pertains to an
- * individual character... Attribute, Name, Description, etc Created on 25NOV08
+ * <code>Character</code> objects should only represent the data that pertains
+ * to an individual character... attribute, name, description, etc.
+ * @author david.h.loman
  */
 public class Character implements Targetable, Persistable {
    private final int charID;
 
-   // info
-   private final String name;
    private final String description;
+   /**
+    * Info.
+    */
+   private final String name;
    private String prompt;
 
-   // stats
-   private final StatMap stats = new StatMap();
-
-   // 'Slots'
+   /**
+    * Slots.
+    */
    private final HashMap<String, ItemContainer> slots = new HashMap<String, ItemContainer>();
+
+   /**
+    * Stats.
+    */
+   private final StatMap stats = new StatMap();
 
    // location
    // private Room room;
 
-   public Character(final int iID, final String name, final String desc) {
+   /**
+    * Explicit constructor.
+    * @param iID
+    *           the ID
+    * @param inName
+    *           the character's name
+    * @param desc
+    *           the description of the character
+    */
+   public Character(final int iID, final String inName, final String desc) {
       this.charID = iID;
-      this.name = name;
+      this.name = inName;
       this.description = desc;
    }
 
+   /**
+    * @return the character ID
+    */
    public final int getCharID() {
       return charID;
    }
 
+   /**
+    * @return the character's description
+    */
    public final String getDescription() {
       return description;
    }
 
+   /**
+    * @see jmud.engine.core.Targetable#getName()
+    * @return the character's name
+    */
    public final String getName() {
       return name;
    }
 
+   /**
+    * @return the system prompt
+    */
    public final String getPrompt() {
       return prompt;
    }
 
+   /**
+    * @return the HashMaps of slots for a container
+    */
    public final HashMap<String, ItemContainer> getSlots() {
       return slots;
    }
 
+   /**
+    * @return the StatMap
+    * @see jmud.engine.core.Targetable#getStatMap()
+    */
    public final StatMap getStatMap() {
       return stats;
    }
