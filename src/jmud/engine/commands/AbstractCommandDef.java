9f71c96c589f4ea5cc3550d49710a70ff78a2b42
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-02 17:29:59 +0000

    Added/revised Javadoc.

--- src/jmud/engine/commands/AbstractCommandDef.java
+++ src/jmud/engine/commands/AbstractCommandDef.java
@@ -2,22 +2,41 @@ package jmud.engine.commands;
 
 import java.util.ArrayList;
 
+/**
+ * @author david.h.loman
+ */
 public abstract class AbstractCommandDef {
+
+   /**
+    * Aliases for the command.
+    */
+   protected ArrayList<String> aliases = new ArrayList<String>();
 
    protected String cmdName = "";
 
-   protected ArrayList<String> aliases = new ArrayList<String>();
-
+   /**
+    * @param cmdName
+    *           the set command name
+    */
    public AbstractCommandDef(final String cmdName) {
       this.cmdName = cmdName;
    }
 
+   /**
+    * @return true at this time
+    */
    public abstract boolean doCmd();
 
+   /**
+    * @return an ArrayList of aliases for the command
+    */
    public final ArrayList<String> getAliases() {
       return aliases;
    }
 
+   /**
+    * @return the command name in String form
+    */
    public final String getCmdName() {
       return cmdName;
    }
