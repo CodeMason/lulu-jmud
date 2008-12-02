cb44a03707ea71db917b0f49a7dd8dc715096058
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-02 17:32:07 +0000

    Added/revised Javadoc.

--- trunk/src/jmud/engine/config/BaseConfig.java
+++ trunk/src/jmud/engine/config/BaseConfig.java
@@ -1,13 +1,22 @@
 package jmud.engine.config;
 
 /**
- * 25NOV08: BaseConfig is used to configure items that the core framework needs.
- * Created on 25NOV08
+ * <code>BaseConfig</code> is used to configure items that the core framework
+ * needs.
+ * @author david.h.loman
  */
 
-public class BaseConfig {
+public final class BaseConfig {
 
    public static void doBaseConfig() {
+
+   }
+
+   /**
+    * <code>BaseConfig</code> is a utility class. Disallowing public/default
+    * constructor.
+    */
+   private BaseConfig() {
 
    }
 
