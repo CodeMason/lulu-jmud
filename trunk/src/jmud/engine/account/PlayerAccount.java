bd7525754da9f505fab4f142746d25b06c001807
samuel.trassare <samuel.trassare@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2008-12-02 16:47:01 +0000

    Added/revised Javadoc.

--- trunk/src/jmud/engine/account/PlayerAccount.java
+++ trunk/src/jmud/engine/account/PlayerAccount.java
@@ -6,47 +6,88 @@ import java.util.Set;
 import jmud.engine.character.Character;
 
 /**
- * 24NOV08: PlayerAccount should only represent the data that pertains to
- * Account only, aka, username, email and password plus any settings they might
- * have, aka Telnet settings. A playerAccount will point to many Characters.
- * Created on 24NOV08
+ * <code>PlayerAccount</code> represents the data that pertains to an account
+ * only, such as the player's username, email and password plus any Telnet
+ * settings a player might have. A <code>PlayerAccount</code> will point to many
+ * Characters.
+ * @author david.h.loman
  */
-
 public class PlayerAccount {
-   private int playerAccountID = -1;
-
-   // validation
-   private String uname = "";
-   private String password = "";
-   private String emailAddress = "";
-
+   /**
+    * The set of characters associated with this PlayerAccount.
+    */
    private final Set<Character> characters = new HashSet<Character>();
 
-   public PlayerAccount(final String emailAddress, final String password,
-         final int playerAccountID, final String uname) {
+   /**
+    * Used for validation of the e-mail address.
+    */
+   private String emailAddress = "";
+   /**
+    * Used for validation of the password.
+    */
+   private String password = "";
+   /**
+    * the playerAccountID is initialized to a default value.
+    */
+   private int playerAccountID = -1;
+
+   /**
+    * Used for validation of the username.
+    */
+   private String uname = "";
+
+   /**
+    * Explicit constructor for the <code>PlayerAccount</code>.
+    * @param inEmailAddress
+    *           the supplied e-mail account
+    * @param inPassword
+    *           the supplied password
+    * @param inPlayerAccountId
+    *           the account ID
+    * @param inUsername
+    *           the supplied username
+    */
+   public PlayerAccount(final String inEmailAddress, final String inPassword,
+         final int inPlayerAccountId, final String inUsername) {
       super();
-      this.emailAddress = emailAddress;
-      this.password = password;
-      this.playerAccountID = playerAccountID;
-      this.uname = uname;
+      this.emailAddress = inEmailAddress;
+      this.password = inPassword;
+      this.playerAccountID = inPlayerAccountId;
+      this.uname = inUsername;
    }
 
+   /**
+    * @return the set of <code>Characters</code> for this
+    *         <code>PlayerAccount</code>
+    */
    public final Set<Character> getCharacters() {
       return characters;
    }
 
+   /**
+    * @return the player's e-mail address
+    */
    public final String getEmailAddress() {
       return emailAddress;
    }
 
+   /**
+    * @return the player's password
+    */
    public final String getPassword() {
       return password;
    }
 
+   /**
+    * @return the account ID
+    */
    public final int getPlayerAccountID() {
       return playerAccountID;
    }
 
+   /**
+    * @return the player's username
+    */
    public final String getUname() {
       return uname;
    }
