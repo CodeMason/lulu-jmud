fa55baf44c8cd2a10f4428a815504c240c2c3137
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2009-02-20 18:58:40 +0000

    Changes to Persistable yeilded changes to Item

--- src/jmud/engine/item/Item.java
+++ src/jmud/engine/item/Item.java
@@ -8,36 +8,42 @@ import jmud.engine.stats.StatMap;
  * @author Chris Maguire
  */
 public class Item implements Targetable, Persistable {
-   private AbstractItemDef behavior;
-   private final String name;
+	private AbstractItemDef behavior;
+	private final String name;
 
-   private final StatMap stats = new StatMap();
+	private final StatMap stats = new StatMap();
 
-   private final int uid;
+	private final int uid;
 
-   public Item(final String name, final int uid) {
-      this.name = name;
-      this.uid = uid;
-   }
+	public Item(final String name, final int uid) {
+		this.name = name;
+		this.uid = uid;
+	}
 
-   public AbstractItemDef getBehavior() {
-      return behavior;
-   }
+	public AbstractItemDef getBehavior() {
+		return behavior;
+	}
 
-   public final String getName() {
-      return name;
-   }
+	public final String getName() {
+		return name;
+	}
 
-   public final StatMap getStatMap() {
-      return stats;
-   }
+	public final StatMap getStatMap() {
+		return stats;
+	}
 
-   public final int getUid() {
-      return uid;
-   }
+	public final int getUid() {
+		return uid;
+	}
 
-   public final void setBehavior(final AbstractItemDef behavior) {
-      this.behavior = behavior;
-   }
+	public final void setBehavior(final AbstractItemDef behavior) {
+		this.behavior = behavior;
+	}
+
+	@Override
+	public boolean save() {
+		// TODO Finish Item.Save()
+		return false;
+	}
 
 }
