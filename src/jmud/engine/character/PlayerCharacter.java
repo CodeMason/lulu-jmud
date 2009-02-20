4e5646b4777f1b15fd148b331ab69fdf6fafbe04
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2009-02-20 18:55:45 +0000

    Changed Character/CharacterManager to PlayerCharacter/PlayerCharacterManager... Java got confused with char's Object type Character ;) Also filled out some routines in the Managers to support the concept of having only the Managers access the DB.

---
+++ src/jmud/engine/character/PlayerCharacter.java
@@ -0,0 +1,71 @@
+package jmud.engine.character;
+
+import jmud.engine.dbio.Persistable;
+import jmud.engine.netIO.Connection;
+import jmud.engine.object.JMudObject;
+
+/**
+ * <code>Character</code> objects should only represent the data that pertains
+ * to an individual character... attribute, name, description, etc.
+ * 
+ * @author david.h.loman
+ */
+public class PlayerCharacter extends JMudObject implements Persistable {
+
+	private Connection c = null;
+	private int ownerAccountID;
+	
+	/**
+	 * Explicit constructor.
+	 * 
+	 * @param iID
+	 *            the ID
+	 * @param inName
+	 *            the character's name
+	 * @param desc
+	 *            the description of the character
+	 */
+	public PlayerCharacter(final String inName) {
+		super(inName);
+
+	}
+
+	public Connection getConnection() {
+		return c;
+	}
+
+	public void setConnection(Connection conn) {
+		this.c = conn;
+	}
+
+	public String getPlayerCharactersName() {
+		return this.getHumanReadableName();
+	}
+	
+	/**
+	 * @return the ownerAccountID
+	 */
+	public int getOwnerAccountID() {
+		return ownerAccountID;
+	}
+
+	/**
+	 * Override the sendToConsole nature of the baseJMudObject and instead, link
+	 * it to the Connection
+	 */
+	@Override
+	public void sendToConsole(String text) {
+		if (this.c != null) {
+			this.c.sendTextLn(text);
+		} else {
+			//Send it to.... where?!?
+		}
+	}
+
+	@Override
+	public boolean save() {
+		// TODO Finish PlayerCharacter.save()
+		return false;
+	}
+
+}
---
+++ src/jmud/engine/character/PlayerCharacterManager.java
@@ -0,0 +1,141 @@
+package jmud.engine.character;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import jmud.engine.character.PlayerCharacter;
+import jmud.engine.dbio.MysqlConnection;
+
+/**
+ * <code>CharacterManager</code> is a singleton patterned class designed to
+ * store and facilitate easy lookup of online characters.
+ * @author david.h.loman
+ */
+public class PlayerCharacterManager {
+   /**
+    * <code>Holder</code> is loaded on the first execution of
+    * <code>CharacterManager.getInstance()</code> or the first access to
+    * <code>Holder.INSTANCE</code>, not before.
+    */
+   private static final class Holder {
+      /**
+       * The singleton instance.
+       */
+      private static final PlayerCharacterManager INSTANCE = new PlayerCharacterManager();
+
+      /**
+       * <code>Holder</code> is a utility class. Disallowing
+       * public/default constructor.
+       */
+      private Holder() {
+
+      }
+   }
+
+   /**
+    * @return the singleton instance
+    */
+   public static PlayerCharacterManager getInstance() {
+      return Holder.INSTANCE;
+   }
+
+   private final HashMap<String, PlayerCharacter> nameMap = new HashMap<String, PlayerCharacter>();
+
+   /**
+    * Protected constructor is sufficient to suppress unauthorized calls to the
+    * constructor.
+    */
+   protected PlayerCharacterManager() {
+   }
+
+
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+	/**
+	 * Load a PlayerCharacter.
+	 * 
+	 * @param pcName
+	 *            the character name of the PlayerCharacter to lookup
+	 * 
+	 * @return Either a reference to an PlayerCharacter object if the DB lookup is a
+	 *         success, or NULL if the DB lookup fails.
+	 */
+	public PlayerCharacter loadPlayerCharacter(String pcName, int ownerAccountID) {
+		// Get the PlayerCharacter data from the database
+		PlayerCharacter pc = MysqlConnection.getPlayerCharacter(pcName, ownerAccountID);
+
+		
+		if (pc == null) {
+			return null;
+		} else {
+			this.nameMap.put(pc.getPlayerCharactersName(), pc);
+			return pc;
+		}
+	}
+
+	/**
+	 * Unload an PlayerCharacter.
+	 * 
+	 * @param pc
+	 *            PlayerCharacter object reference to be unloaded
+	 * 
+	 * @return boolean value showing whether or not the PlayerCharacter object was
+	 *         unloaded from the PlayerCharacterManager successfully.
+	 */
+	public boolean unloadAccont(PlayerCharacter pc) {
+		return this.unloadPlayerCharacter(pc.getPlayerCharactersName());
+	}
+
+	/**
+	 * Unload an PlayerCharacter.
+	 * 
+	 * @param PlayerCharacterID
+	 *            Integer value representing the ID of the PlayerCharacter object to be
+	 *            unloaded.
+	 * 
+	 * @return boolean value showing whether or not the PlayerCharacter object was
+	 *         unloaded from the PlayerCharacterManager successfully.
+	 */
+	public boolean unloadPlayerCharacter(String pcName) {
+		PlayerCharacter pc = this.nameMap.remove(pcName);
+		if (pc == null) {
+			System.err.println("Failed to obtain PlayerCharacter object reference during unloadPlayerCharacter()");
+			return true;
+		}
+		if (pc.save() == false) {
+			System.err.println("PlayerCharacter failed to .save() during unloadPlayerCharacter()!!!!!");
+			return true;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Gets a reference to a PlayerCharacter object.
+	 * 
+	 * @param pcName
+	 *            Integer value representing the ID of the PlayerCharacter object to be returned.
+	 * 
+	 * @return Either a reference to an PlayerCharacter object if the PlayerCharacterManager lookup is a
+	 *         success, or NULL if the PlayerCharacterManager lookup fails.
+	 */
+	public PlayerCharacter getPlayerCharacter(String pcName) {
+		return this.nameMap.get(pcName);
+	}
+	
+	
+	public Set<String> getAllPlayersOnline() {
+		return this.nameMap.keySet();
+	}
+
+}
