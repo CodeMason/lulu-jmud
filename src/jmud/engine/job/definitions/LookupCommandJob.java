7bec1dfaf376e086f8ea7f83d0028284cfb26b31
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2009-02-22 00:43:12 +0000

    The keystone to the bridge between networkIO and the Command subsystem.  If a connection's ConnectionState is INGAME, then the string parsed from the Connection's COmmandBuffer is placed into this LookupCommandJob.  The LookupCommandJob does just what it sounds like, it attempts to lookup a command registered with the CommandManager.

---
+++ src/jmud/engine/job/definitions/LookupCommandJob.java
@@ -0,0 +1,56 @@
+/**
+ * This file is part of Lulu's JMud.
+ *
+ *  Lulu's JMud is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Lulu's JMud is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Lulu's JMud.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmud.engine.job.definitions;
+
+import jmud.engine.account.Account;
+import jmud.engine.character.PlayerCharacter;
+import jmud.engine.character.PlayerCharacterManager;
+import jmud.engine.netIO.Connection;
+
+/**
+ * Just a template. Can be deleted once the Job Repository has sufficient
+ * samples to draw from.
+ * 
+ * @author David Loman
+ * @version 0.1
+ */
+
+public class LookupCommandJob extends AbstractConnectionJob {
+	
+	private String pcName;
+	
+	public LookupCommandJob(Connection c, String pcName) {
+		super(c);
+		this.pcName = pcName;
+	}
+
+	@Override
+	public final boolean doJob() {
+		synchronized (this.c) {
+			Account a = this.c.getAccount();
+			
+			PlayerCharacter pc = PlayerCharacterManager.getInstance().loadPlayerCharacter(this.pcName, a.getAccountID());
+			
+			if (pc == null) {
+				return false;
+			} else {
+				this.c.setPc(pc);
+				return true;
+			}
+		}
+	}
+}
