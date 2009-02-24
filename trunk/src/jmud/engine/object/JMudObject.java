91b150bcf78a7f4203a70d2bada8d6b97e1ffd60
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2009-02-24 12:00:19 +0000

    JMudObject was getting a bit complex with all the Mappings.  Broke down the Maps into their own *Map objects.  All functionality remains the same, including internal syncho.

--- trunk/src/jmud/engine/object/JMudObject.java
+++ trunk/src/jmud/engine/object/JMudObject.java
@@ -16,9 +16,8 @@
  */
 package jmud.engine.object;
 
-import jmud.engine.attribute.Attribute;
-import jmud.engine.behavior.AbstractBehavior;
-import jmud.engine.event.JMudEventType;
+import jmud.engine.attribute.AttributeMap;
+import jmud.engine.behavior.BehaviorMap;
 
 import java.util.*;
 
@@ -39,33 +38,11 @@ public class JMudObject {
 	 */
 	private String displayedName = "";
 
-	/**
-	 * A mapping of sibling JMudObjects and their relation
-	 */
-	private Map<String, JMudObject> siblingConnectionMap = Collections
-			.synchronizedMap(new HashMap<String, JMudObject>());
-
-	/**
-	 * A set of child JMudObjects
-	 */
-	private Set<JMudObject> childObjects = Collections.synchronizedSet(new HashSet<JMudObject>());
-
-	/**
-	 * Reference to a Parent JMudObject, if one exists
-	 */
-	private JMudObject parentObject;
-
-	/**
-	 * A mapping of events to behaviors
-	 */
-	private Map<JMudEventType, AbstractBehavior> behaviorMap = Collections
-			.synchronizedMap(new HashMap<JMudEventType, AbstractBehavior>());
-
-	/**
-	 * A mapping of Attribute names to Attribute references
-	 */
-	private Map<String, Attribute> attributeMap = Collections.synchronizedMap(new HashMap<String, Attribute>());
-
+	private BehaviorMap behaviorMap;
+	private AttributeMap attributeMap;
+	private JMudObjectRelationMap JmoRelationMap;
+	
+	
 	/*
 	 * Constructors
 	 */
@@ -75,60 +52,25 @@ public class JMudObject {
 
 	public JMudObject(UUID uuid) {
 		this.uuid = uuid;
+		this.attributeMap = new AttributeMap();
 	}
 
+	
 	/*
-	 * Sibling Map routines
+	 * Getters n Setters
 	 */
+	public AttributeMap getAttributeMap(){
+		return this.attributeMap;
+	}
+	public BehaviorMap getBehaviorMap(){
+		return this.behaviorMap;
+	}
+	public JMudObjectRelationMap getJmoRelationMap(){
+		return this.JmoRelationMap;
+	}
+	
+	
 
-	public Set<String> getRelations() {
-		return this.siblingConnectionMap.keySet();
-	}
-
-	public JMudObject getSiblingByRelation(String relation) {
-		return this.siblingConnectionMap.get(relation);
-	}
-
-	public Set<JMudObject> getAllSibling() {
-		return (Set<JMudObject>) this.siblingConnectionMap.values();
-	}
-
-	public void addSibling(String relation, JMudObject sibling) {
-		this.siblingConnectionMap.put(relation, sibling);
-	}
-
-	public JMudObject remSiblingByRelation(String relation) {
-		return this.siblingConnectionMap.remove(relation);
-	}
-
-	/*
-	 * Parent JMudObject Getter/Setter
-	 */
-	public JMudObject getParentObject() {
-		return this.parentObject;
-	}
-
-	public void setParentObject(JMudObject parent) {
-		this.parentObject = parent;
-	}
-
-	/*
-	 * Child JMudObject Set routines
-	 */
-	public boolean addChild(JMudObject jmo) {
-		return this.childObjects.add(jmo);
-	}
-
-	public boolean hasChild(JMudObject jmo) {
-		return this.childObjects.contains(jmo);
-	}
-
-	public boolean remove(JMudObject jmo) {
-		return this.childObjects.remove(jmo);
-	}
-	public Set<JMudObject> getAllChildren() {
-		return this.childObjects;
-	}
 
 	/*
 	 * UUID Getter
@@ -148,64 +90,6 @@ public class JMudObject {
 		this.displayedName = displayedName;
 	}
 
-	/*
-	 * Attribute Map Routines
-	 */
-	public boolean containsAttribute(String attrName) {
-		return this.attributeMap.containsKey(attrName);
-	}
-
-	public boolean containsAttribute(Attribute a) {
-		return this.attributeMap.containsValue(a);
-	}
-
-	public Attribute getAttribute(String attrName) {
-		return this.attributeMap.get(attrName);
-	}
-
-	public Set<String> getAttributeNames() {
-		return this.attributeMap.keySet();
-	}
-
-	public Set<Attribute> getAttributes() {
-		return (Set<Attribute>) this.attributeMap.values();
-	}
-
-	public Attribute addAttribute(String name, Attribute a) {
-		return this.attributeMap.put(name, a);
-	}
-
-	public Attribute remAttribute(String name) {
-		return this.attributeMap.remove(name);
-	}
-
-	/*
-	 * Behavior Map Routines
-	 */
-
-	public boolean containsBehavior(AbstractBehavior b) {
-		return behaviorMap.containsValue(b);
-	}
-
-	public AbstractBehavior getBehavior(JMudEventType jmet) {
-		return behaviorMap.get(jmet);
-	}
-
-	public Set<JMudEventType> getEventTypes() {
-		return behaviorMap.keySet();
-	}
-
-	public AbstractBehavior addBehavior(JMudEventType jmet, AbstractBehavior b) {
-		return behaviorMap.put(jmet, b);
-	}
-
-	public AbstractBehavior removeBehavior(JMudEventType jmet) {
-		return behaviorMap.remove(jmet);
-	}
-
-	public Set<AbstractBehavior> getBehaviors() {
-		return (Set<AbstractBehavior>) behaviorMap.values();
-	}
 
 	
 	/*
---
+++ trunk/src/jmud/engine/object/JMudObjectRelationMap.java
@@ -0,0 +1,78 @@
+package jmud.engine.object;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class JMudObjectRelationMap {
+	/**
+	 * A mapping of sibling JMudObjects and their relation
+	 */
+	private Map<String, JMudObject> siblingConnectionMap = Collections
+			.synchronizedMap(new HashMap<String, JMudObject>());
+
+	/**
+	 * A set of child JMudObjects
+	 */
+	private Set<JMudObject> childObjects = Collections.synchronizedSet(new HashSet<JMudObject>());
+
+	/**
+	 * Reference to a Parent JMudObject, if one exists
+	 */
+	private JMudObject parentObject;
+
+	/*
+	 * Sibling Map routines
+	 */
+
+	public Set<String> getAllSiblingRelations() {
+		return this.siblingConnectionMap.keySet();
+	}
+
+	public JMudObject getSiblingByRelation(String relation) {
+		return this.siblingConnectionMap.get(relation);
+	}
+
+	public Set<JMudObject> getAllSiblings() {
+		return (Set<JMudObject>) this.siblingConnectionMap.values();
+	}
+
+	public void addSibling(String relation, JMudObject sibling) {
+		this.siblingConnectionMap.put(relation, sibling);
+	}
+
+	public JMudObject remSiblingByRelation(String relation) {
+		return this.siblingConnectionMap.remove(relation);
+	}
+
+	/*
+	 * Parent JMudObject Getter/Setter
+	 */
+	public JMudObject getParentObject() {
+		return this.parentObject;
+	}
+
+	public void setParentObject(JMudObject parent) {
+		this.parentObject = parent;
+	}
+
+	/*
+	 * Child JMudObject Set routines
+	 */
+	public boolean addChild(JMudObject jmo) {
+		return this.childObjects.add(jmo);
+	}
+
+	public boolean hasChild(JMudObject jmo) {
+		return this.childObjects.contains(jmo);
+	}
+
+	public boolean remChild(JMudObject jmo) {
+		return this.childObjects.remove(jmo);
+	}
+	public Set<JMudObject> getAllChildren() {
+		return this.childObjects;
+	}
+}
