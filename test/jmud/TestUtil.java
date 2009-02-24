272373be6cdb798941d43e596b10278dd5a540ac
dloman77 <dloman77@e7f9995b-9541-0410-b4c3-5fd4048e9d37>
2009-02-24 16:44:28 +0000

    Restored a file and fixed it up!

--- test/jmud/TestUtil.java
+++ test/jmud/TestUtil.java
@@ -17,16 +17,10 @@
 package jmud;
 
 import jmud.engine.object.JMudObject;
-import jmud.engine.behavior.*;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Arrays;
 
 public class TestUtil{
-    public static final int MILLIS_TO_ALLOW_EVENT_COMPLETION = 100;
-    public static final int MILLIS_TO_ALLOW_WORKER_WAKEUP = 250;
-    private static final List<String> STANDARD_GEAR_NAMES = Arrays.asList("Sword", "Shield", "Helmet");
 
     public static JMudObject buildSimpleJMudObjectTree() {
 		JMudObject root = new JMudObject("root");
@@ -36,22 +30,24 @@ public class TestUtil{
 		JMudObject orc1 = new JMudObject("orc1");
 		JMudObject chair = new JMudObject("chair");
 		JMudObject bag = new JMudObject("bag");
+		
         // QQQ CM: multiple coins as one object? Is this a design decision, or simply a name?
 		// AAA DHL: Nah, just a place holder.  Dunno how we are going to implement large qtys of the same item...
+		
         JMudObject goldcoins = new JMudObject("goldcoins");
 		JMudObject mapOfDungeon = new JMudObject("mapOfDungeon");
 		JMudObject door = new JMudObject("door");
 
-		root.addChildObject(room);
-		room.addChildObject(pcSteve);
-		room.addChildObject(orc0);
-		room.addChildObject(orc1);
-		room.addChildObject(chair);
-		room.addChildObject(bag);
-		room.addChildObject(door);
+		root.getJmoRelMap().addChild(room);
+		room.getJmoRelMap().addChild(pcSteve);
+		room.getJmoRelMap().addChild(orc0);
+		room.getJmoRelMap().addChild(orc1);
+		room.getJmoRelMap().addChild(chair);
+		room.getJmoRelMap().addChild(bag);
+		room.getJmoRelMap().addChild(door);
 
-		bag.addChildObject(goldcoins);
-		bag.addChildObject(mapOfDungeon);
+		bag.getJmoRelMap().addChild(goldcoins);
+		bag.getJmoRelMap().addChild(mapOfDungeon);
 
 		return root;
 	}
@@ -60,39 +56,15 @@ public class TestUtil{
 
         JMudObject root = new JMudObject("root");
         JMudObject room = new JMudObject("room");
-        root.addChildObject(room);
+        root.getJmoRelMap().addChild(room);
 
         for(int i = 0; i < numberOfParentObjects; i++){
-            room.addChildObject(createOrcWithGearAndBehaviors());
+//            room.getJmoRelMap().addChild(createOrcWithGearAndBehaviors());
         }
 
         return root;
     }
 
-    private static JMudObject createOrcWithGearAndBehaviors(){
-        JMudObject orc = new JMudObject("Orc");
-        addBehaviorsToObject(orc, Arrays.asList((Class) GetBehavior.class, GotBehavior.class, AttackBehavior.class, AttackedBehavior.class));
-        addGearObjectsWithBehaviors(orc, STANDARD_GEAR_NAMES);
-        return orc;
-    }
-
-    private static void addGearObjectsWithBehaviors(JMudObject parentObject, List<String> gearNames){
-        for(String gearName : gearNames){
-            parentObject.addChildObject(createGearObjectWithBehaviors(gearName));
-        }
-    }
-
-    private static JMudObject createGearObjectWithBehaviors(String childObjectName){
-        JMudObject newObject = new JMudObject(childObjectName);
-        addBehaviorsToObject(newObject, Arrays.asList((Class) GetBehavior.class, GotBehavior.class));
-        return newObject;
-    }
-
-    private static void addBehaviorsToObject(JMudObject object, List<Class> behaviorClassesToAdd){
-        for(AbstractBehavior behavior : BehaviorRegistrar.createBehaviorsFromClasses(behaviorClassesToAdd, object)){
-            object.registerBehaviorForEventTypesHandled(behavior);
-        }
-    }
 
     public static void printJMudObjectTree(JMudObject root, String debugMessage){
         System.out.println("\n\n" + debugMessage);
@@ -125,7 +97,7 @@ public class TestUtil{
 		System.out.println(s.toString());
 
 		// recurse on all children
-		for (JMudObject j : jmo.getChildObjects().values()) {
+		for (JMudObject j : jmo.getJmoRelMap().getAllChildren()) {
 			TestUtil.printJMudObjectTree(j, lvl + 1);
 		}
 
